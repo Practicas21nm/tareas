6625a4890859d8f1ce50c4a3e1220a12a2bfcf2a
Tareas Trabajos <tareasypracticas5zt@gmail.com>
2023-01-18 16:33:25 -0600

    Se realiza la suma de dos enteros

--- s/suma.c
+++ s/suma.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
-int main()
-{
+int main() {
 	int numero = 10;
+	int a, b;
 	printf("%d\n", numero + numero);
 	printf("Tercera impresion\n");
+
+	printf("Por favor escriba dos enteros.\n");
+	scanf("%d %d");
+	printf("El resultado de la suma es %d\n", a + b);
 
 	return 0;
 }
